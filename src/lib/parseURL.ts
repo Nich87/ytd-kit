cebd9d43e855f0155271ade442c0e64217542091
ysr9029 <56439343+Nich87@users.noreply.github.com>
2023-09-18 20:30:48 +0900

    feat:add error handling

--- src/lib/parseURL.ts
+++ src/lib/parseURL.ts
@@ -1,22 +1,25 @@
 export function parseVideoUrl(url: string): string | null {
-    if(!(url.startsWith("https://") || url.startsWith("https://"))) url = "https://" + url;
-  const parsed = new URL(url);
+	if (!url.startsWith('https://')) url = 'https://' + url;
+	try {
+		const parsed = new URL(url);
+		const [, one, two] = parsed.pathname.split('/');
+		if (parsed.hostname === 'youtu.be') return one;
 
-  const [, one, two] = parsed.pathname.split("/");
-  if (parsed.hostname === "youtu.be") return one;
+		const [com, youtube] = parsed.hostname.split('.').reverse();
+		if (com !== 'com' || youtube !== 'youtube') return null;
 
-  const [com, youtube] = parsed.hostname.split(".").reverse();
-  if (com !== "com" || youtube !== "youtube") return null;
-
-  switch (one) {
-    case "v":
-    case "videos":
-    case "embed":
-    case "shorts":
-      return two;
-    case "watch":
-      return parsed.searchParams.get("v");
-  }
-
-  return parsed.searchParams.get("v");
+		switch (one) {
+			case 'v':
+			case 'videos':
+			case 'embed':
+			case 'shorts':
+				return two;
+			case 'watch':
+				return parsed.searchParams.get('v');
+		}
+		return parsed.searchParams.get('v');
+	} catch (e) {
+		console.error(e);
+		return null;
+	}
 }
