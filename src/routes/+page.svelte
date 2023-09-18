814de4548055e3a9a66fb44260e6b0038b6fdc3a
ysr9029 <56439343+Nich87@users.noreply.github.com>
2023-09-18 16:56:03 +0900

    refactor: optimize mobile responsive code

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -76,116 +76,125 @@
 	}
 </script>
 
-<div class="mx-auto flex flex-col justify-center items-center px-6 pt-8 pt:mt-0">
-	<div class="border-solid border-2 border-sky-500 w-1/2">
+<div class="container mx-auto p-4">
+	<div class="max-w-3xl mx-auto border-solid border-2 border-sky-500 p-4">
 		<Tabs>
 			<TabItem open title="from URL">
-				<Input
-					type="text"
-					bind:value={url}
-					placeholder="https://www.youtube.com/watch?v=xxxxxxx"
-					pattern="^((?:https?:)?\/\/)?((?:www|m)\.)?((?:youtube\.com|youtu.be))(\/(?:[\w\-]+\?v=|embed\/|v\/)?)([\w\-]+)(\S+)?$"
-					required
-				/>
-				<Button on:click={searchVideoInfo}>Search</Button>
+				<div class="flex flex-col items-center">
+					<Input
+						type="text"
+						bind:value={url}
+						placeholder="https://www.youtube.com/watch?v=xxxxxxx"
+						pattern="^((?:https?:)?\/\/)?((?:www|m)\.)?((?:youtube\.com|youtu.be))(\/(?:[\w\-]+\?v=|embed\/|v\/)?)([\w\-]+)(\S+)?$"
+						required
+					/>
+					<Button on:click={searchVideoInfo}>Search</Button>
+				</div>
 			</TabItem>
 
 			<TabItem title="from Search">
-				<Input type="text" bind:value={url} placeholder="君に届け OP" required />
-				<Button on:click={searchVideoInfo}>Search</Button>
+				<div class="flex flex-col items-center">
+					<Input type="text" bind:value={url} placeholder="君に届け OP" required />
+					<Button on:click={searchVideoInfo}>Search</Button>
+				</div>
 			</TabItem>
 
 			<TabItem title="from Playlist">
-				<Input
-					type="text"
-					bind:value={url}
-					placeholder="https://youtube.com/playlist?list=xxxxxxxx"
-					required
-				/>
-				<Button on:click={searchPlaylistInfo}>Search</Button>
+				<div class="flex flex-col items-center">
+					<Input
+						type="text"
+						bind:value={url}
+						placeholder="https://youtube.com/playlist?list=xxxxxxxx"
+						required
+					/>
+					<Button on:click={searchPlaylistInfo}>Search</Button>
+				</div>
 			</TabItem>
 		</Tabs>
 	</div>
-</div>
 
-{#if videoInfo}
-	<div class="mx-auto flex flex-col justify-center items-center px-6 pt-8 pt:mt-0">
-		<div class="flex space-x-4">
-			<Button on:click={() => downloadVideo(videoInfo.videoId, 'video')}
-				><VideoSolid class="m-1" />
-				Download Video</Button
-			>
-			<Button on:click={() => downloadVideo(videoInfo.videoId, 'audio')}
-				><FileMusicSolid class="m-1" />Download Audio</Button
-			>
-		</div>
-		<Hr />
-		<div>
-			<iframe
-				title={videoInfo.title}
-				src={videoInfo.iframe.iframeUrl}
-				width={videoInfo.iframe.iframeWidth}
-				height={videoInfo.iframe.iframeHeight}
-			></iframe>
-			<P size="2xl" weight="semibold">{videoInfo.title}</P>
-			<P size="xl" weight="semibold">Category:{videoInfo.category}</P>
-			<P size="xl" weight="semibold">👀{videoInfo.counts.viewCount}</P>
-			<P size="xl" weight="semibold">👍{videoInfo.counts.likeCount}</P>
-			<Hr />
-			<Blockquote border bg class="p-4 my-4">
-				<pre>{videoInfo.description}</pre>
-			</Blockquote>
-			<Hr />
-			<div class="flex">
-				{#each videoInfo.keywords as keyword}
-					<Badge
-						border
-						color="red"
-						href="https://www.youtube.com/results?search_query={keyword}"
-						rel="noopener noreferrer"
-						target="_blank"><SearchOutline class="w-2.5 h-2.5 mr-1.5" />{keyword}</Badge
-					>
-				{/each}
-				<Hr />
+	{#if videoInfo}
+		<div class="max-w-3xl mx-auto mt-8">
+			<div class="flex justify-center space-x-4">
+				<Button on:click={() => downloadVideo(videoInfo.videoId, 'video')}
+					><VideoSolid class="m-1" />
+					Download Video</Button
+				>
+				<Button on:click={() => downloadVideo(videoInfo.videoId, 'audio')}
+					><FileMusicSolid class="m-1" />Download Audio</Button
+				>
+			</div>
+			<Hr class="my-4" />
+			<div>
+				<iframe
+					title={videoInfo.title}
+					src={videoInfo.iframe.iframeUrl}
+					width="100%"
+					height={videoInfo.iframe.iframeHeight}
+				/>
+				<P size="2xl" weight="semibold">{videoInfo.title}</P>
+				<P size="xl" weight="semibold">Category: {videoInfo.category}</P>
+				<P size="xl" weight="semibold">👀 {videoInfo.counts.viewCount}</P>
+				<P size="xl" weight="semibold">👍 {videoInfo.counts.likeCount}</P>
+				<Hr class="my-4" />
+				<Blockquote border bg class="p-4 overflow-auto">
+					<pre class="whitespace-pre-wrap">{videoInfo.description}</pre>
+				</Blockquote>
+
+				<Hr class="my-4" />
+				<div class="flex flex-wrap items-center">
+					{#each videoInfo.keywords as keyword}
+						<Badge
+							border
+							color="red"
+							href="https://www.youtube.com/results?search_query={keyword}"
+							rel="noopener noreferrer"
+							target="_blank"
+							class="mr-2 mb-2"><SearchOutline class="w-2.5 h-2.5 mr-1.5" />{keyword}</Badge
+						>
+					{/each}
+				</div>
 			</div>
 		</div>
-	</div>
-{/if}
+	{/if}
 
-{#if playlistInfo}
-	<div class="flex justify-center items-center">
-		<img
-			src={playlistInfo.author.bestAvatar.url}
-			width={playlistInfo.author.bestAvatar.width}
-			height={playlistInfo.author.bestAvatar.height}
-			alt="CoverImage"
-		/>
-		<div>
-			<p>Playlist:{playlistInfo.title}</p>
-			<p>Author:{playlistInfo.author.name} | item:{playlistInfo.itemCount}</p>
+	{#if playlistInfo}
+		<div class="max-w-3xl mx-auto mt-8">
+			<div class="flex flex-col items-center">
+				<img
+					src={playlistInfo.author.bestAvatar.url}
+					width={playlistInfo.author.bestAvatar.width}
+					height={playlistInfo.author.bestAvatar.height}
+					alt="CoverImage"
+				/>
+				<div>
+					<p>Playlist: {playlistInfo.title}</p>
+					<p>Author: {playlistInfo.author.name} | item: {playlistInfo.itemCount}</p>
+				</div>
+			</div>
+			<div class="border-solid border-2 border-red-500 mt-4">
+				<Listgroup>
+					{#each playlistInfo.videos as video}
+						<ListgroupItem class="flex items-center">
+							<a href={video.url} class="m-1">{video.title}</a>
+							<VideoSolid class="m-1" on:click={() => downloadVideo(video.videoId, 'video')} />
+							<FileMusicSolid class="m-1" on:click={() => downloadVideo(video.videoId, 'audio')} />
+						</ListgroupItem>
+					{/each}
+				</Listgroup>
+			</div>
 		</div>
-	</div>
-	<div class="border-solid border-2 border-red-500">
-		<Listgroup>
-			{#each playlistInfo.videos as video}
-				<ListgroupItem class="flex">
-					<a href={video.url} class="m-1">{video.title}</a>
-					<VideoSolid class="m-1" on:click={() => downloadVideo(video.videoId, 'video')} />
-					<FileMusicSolid class="m-1" on:click={() => downloadVideo(video.videoId, 'audio')} />
-				</ListgroupItem>
-			{/each}
-		</Listgroup>
-	</div>
-{/if}
+	{/if}
 
-{#if popupModal}
-	<Modal bind:open={popupModal} size="xs" autoclose outsideclose>
-		<div class="text-center">
-			<ExclamationCircleOutline class="mx-auto mb-4 text-gray-400 w-12 h-12 dark:text-gray-200" />
-			<h3 class="mb-5 text-lg font-normal text-gray-500 dark:text-gray-400">
-				不正なURLです。修正し再度お試しください。
-			</h3>
-			<Button color="red" class="mr-2">OK</Button>
-		</div>
-	</Modal>
-{/if}
+	{#if popupModal}
+		<Modal bind:open={popupModal} size="xs" autoclose outsideclose>
+			<div class="text-center">
+				<ExclamationCircleOutline class="mx-auto mb-4 text-gray-400 w-12 h-12 dark:text-gray-200" />
+				<h3 class="mb-5 text-lg font-normal text-gray-500 dark:text-gray-400">
+					不正なURLです。修正し再度お試しください。
+				</h3>
+				<Button color="red" class="mr-2">OK</Button>
+			</div>
+		</Modal>
+	{/if}
+</div>
