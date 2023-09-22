ee65bcb617fe9975bcb987722ad0b6b54c3496e2
ysr9029 <56439343+Nich87@users.noreply.github.com>
2023-09-23 01:40:28 +0900

    feat:added DarkMode

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -8,9 +8,16 @@
 		P,
 		Hr,
 		Avatar,
-		Card
+		Card,
+		DarkMode
 	} from 'flowbite-svelte';
-	import { VideoSolid, FileMusicSolid, SearchOutline } from 'flowbite-svelte-icons';
+	import {
+		VideoSolid,
+		FileMusicSolid,
+		SearchOutline,
+		SunSolid,
+		MoonSolid
+	} from 'flowbite-svelte-icons';
 	import { parseVideoUrl } from '$lib/parseURL';
 	import type { VideoInfo, PlaylistInfo, SearchInfo } from '$lib/types/index';
 	import URLErrorModal from 'components/Modals/URLError.svelte';
@@ -107,7 +114,16 @@
 	}
 </script>
 
-<div class="container mx-auto p-4">
+<DarkMode class="fixed right-0">
+	<svelte:fragment slot="lightIcon">
+		<SunSolid />
+	</svelte:fragment>
+	<svelte:fragment slot="darkIcon">
+		<MoonSolid />
+	</svelte:fragment>
+</DarkMode>
+
+<div class="container mx-auto p-4 space-y-8">
 	<div class="max-w-3xl mx-auto border-solid border-2 border-sky-500 p-4">
 		<MainTabs
 			on:Video={(e) => {
