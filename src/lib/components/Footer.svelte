f47553cc4c0976741bbaa92f9ffe7d8922f9aa65
ysr9029 <56439343+Nich87@users.noreply.github.com>
2023-09-25 00:45:47 +0900

    style:code format

--- src/lib/components/Footer.svelte
+++ src/lib/components/Footer.svelte
@@ -1,29 +1,37 @@
 <script lang="ts">
-  import { Footer, FooterCopyright, FooterIcon } from 'flowbite-svelte';
-  import { DiscordSolid, TwitterSolid, GithubSolid, UserCircleSolid } from 'flowbite-svelte-icons';
+	import { Footer, FooterCopyright, FooterIcon } from 'flowbite-svelte';
+	import { DiscordSolid, TwitterSolid, GithubSolid, UserCircleSolid } from 'flowbite-svelte-icons';
 </script>
 
 <div class="flex justify-center w-full flex-grow">
-  <Footer class="mt-auto">
-    <div class="flex flex-col items-center justify-between">
-      <FooterCopyright
-        href="https://github.com/Nich87/ytd-kit/blob/main/LICENSE"
-        by="Nich87(ysr9029)"
-      />
-      <div class="flex mt-4 space-x-6 justify-center py-2 sm:mt-0">
-        <FooterIcon href="https://twitter.com/const_root">
-          <TwitterSolid class="w-4 h-4 text-gray-500 dark:text-gray-500 hover:text-gray-900 dark:hover:text-white" />
-        </FooterIcon>
-        <FooterIcon href="https://discord.com/users/484953715225133057">
-          <DiscordSolid class="w-4 h-4 text-gray-500 dark:text-gray-500 hover:text-gray-900 dark:hover:text-white" />
-        </FooterIcon>
-        <FooterIcon href="https://github.com/Nich87">
-          <GithubSolid class="w-4 h-4 text-gray-500 dark:text-gray-500 hover:text-gray-900 dark:hover:text-white" />
-        </FooterIcon>
-        <FooterIcon href="https://voidhacker.com">
-          <UserCircleSolid class="w-4 h-4 text-gray-500 dark:text-gray-500 hover:text-gray-900 dark:hover:text-white" />
-        </FooterIcon>
-      </div>
-    </div>
-  </Footer>
+	<Footer class="mt-auto">
+		<div class="flex flex-col items-center justify-between">
+			<FooterCopyright
+				href="https://github.com/Nich87/ytd-kit/blob/main/LICENSE"
+				by="Nich87(ysr9029)"
+			/>
+			<div class="flex mt-4 space-x-6 justify-center py-2 sm:mt-0">
+				<FooterIcon href="https://twitter.com/const_root">
+					<TwitterSolid
+						class="w-4 h-4 text-gray-500 dark:text-gray-500 hover:text-gray-900 dark:hover:text-white"
+					/>
+				</FooterIcon>
+				<FooterIcon href="https://discord.com/users/484953715225133057">
+					<DiscordSolid
+						class="w-4 h-4 text-gray-500 dark:text-gray-500 hover:text-gray-900 dark:hover:text-white"
+					/>
+				</FooterIcon>
+				<FooterIcon href="https://github.com/Nich87">
+					<GithubSolid
+						class="w-4 h-4 text-gray-500 dark:text-gray-500 hover:text-gray-900 dark:hover:text-white"
+					/>
+				</FooterIcon>
+				<FooterIcon href="https://voidhacker.com">
+					<UserCircleSolid
+						class="w-4 h-4 text-gray-500 dark:text-gray-500 hover:text-gray-900 dark:hover:text-white"
+					/>
+				</FooterIcon>
+			</div>
+		</div>
+	</Footer>
 </div>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -107,147 +107,144 @@
 	}
 </script>
 
-	<Header />
+<Header />
 
-	<div class="max-w-3xl mx-auto border-solid border-2 border-sky-500 p-4">
-		<MainTabs
-			on:Video={(e) => {
-				url = e.detail;
-				searchVideoInfo();
-			}}
-			on:Playlist={(e) => {
-				url = e.detail;
-				searchPlaylistInfo();
-			}}
-			on:Query={(e) => {
-				url = e.detail;
-				searchQueryInfo();
-			}}
-		/>
-	</div>
+<div class="max-w-3xl mx-auto border-solid border-2 border-sky-500 p-4">
+	<MainTabs
+		on:Video={(e) => {
+			url = e.detail;
+			searchVideoInfo();
+		}}
+		on:Playlist={(e) => {
+			url = e.detail;
+			searchPlaylistInfo();
+		}}
+		on:Query={(e) => {
+			url = e.detail;
+			searchQueryInfo();
+		}}
+	/>
+</div>
 
-	<Spinner />
+<Spinner />
 
-	{#if videoInfo}
-		<div class="max-w-3xl mx-auto mt-8">
-			<div class="flex justify-center space-x-4">
-				<Button on:click={() => downloadVideo(videoInfo.videoId, 'video')}
-					><VideoSolid class="m-1" />
-					Download Video</Button
-				>
-				<Button on:click={() => downloadVideo(videoInfo.videoId, 'audio')}
-					><FileMusicSolid class="m-1" />Download Audio</Button
-				>
-			</div>
+{#if videoInfo}
+	<div class="max-w-3xl mx-auto mt-8">
+		<div class="flex justify-center space-x-4">
+			<Button on:click={() => downloadVideo(videoInfo.videoId, 'video')}
+				><VideoSolid class="m-1" />
+				Download Video</Button
+			>
+			<Button on:click={() => downloadVideo(videoInfo.videoId, 'audio')}
+				><FileMusicSolid class="m-1" />Download Audio</Button
+			>
+		</div>
+		<Hr class="my-4" />
+		<div>
+			<iframe
+				title={videoInfo.title}
+				src={videoInfo.iframe.iframeUrl}
+				width="100%"
+				height={videoInfo.iframe.iframeHeight}
+			/>
+			<P size="2xl" weight="semibold">{videoInfo.title}</P>
+			<P size="xl" weight="semibold">Category: {videoInfo.category}</P>
+			<P size="xl" weight="semibold">👀 {videoInfo.counts.viewCount}</P>
+			<P size="xl" weight="semibold">👍 {videoInfo.counts.likeCount}</P>
 			<Hr class="my-4" />
+			<Blockquote border bg class="p-4 overflow-auto">
+				<pre class="whitespace-pre-wrap">{videoInfo.description}</pre>
+			</Blockquote>
+
+			<Hr class="my-4" />
+			<div class="flex flex-wrap items-center">
+				{#each videoInfo.keywords as keyword}
+					<Badge
+						border
+						color="red"
+						href="https://www.youtube.com/results?search_query={keyword}"
+						rel="noopener noreferrer"
+						target="_blank"
+						class="mr-2 mb-2"><SearchOutline class="w-2.5 h-2.5 mr-1.5" />{keyword}</Badge
+					>
+				{/each}
+			</div>
+		</div>
+	</div>
+{/if}
+
+{#if playlistInfo}
+	<div class="max-w-3xl mx-auto mt-8">
+		<div class="flex flex-col items-center">
+			<img
+				src={playlistInfo.author.bestAvatar.url}
+				width={playlistInfo.author.bestAvatar.width}
+				height={playlistInfo.author.bestAvatar.height}
+				alt="CoverImage"
+			/>
 			<div>
-				<iframe
-					title={videoInfo.title}
-					src={videoInfo.iframe.iframeUrl}
-					width="100%"
-					height={videoInfo.iframe.iframeHeight}
-				/>
-				<P size="2xl" weight="semibold">{videoInfo.title}</P>
-				<P size="xl" weight="semibold">Category: {videoInfo.category}</P>
-				<P size="xl" weight="semibold">👀 {videoInfo.counts.viewCount}</P>
-				<P size="xl" weight="semibold">👍 {videoInfo.counts.likeCount}</P>
-				<Hr class="my-4" />
-				<Blockquote border bg class="p-4 overflow-auto">
-					<pre class="whitespace-pre-wrap">{videoInfo.description}</pre>
-				</Blockquote>
+				<P>Playlist: {playlistInfo.title}</P>
+				<P>Author: {playlistInfo.author.name} | item: {playlistInfo.itemCount}</P>
+			</div>
+		</div>
+		<div class="border-solid border-2 border-red-500 mt-4">
+			<Listgroup>
+				{#each playlistInfo.videos as video}
+					<ListgroupItem class="flex items-center space-between">
+						<a href={video.url} class="m-1 flex-grow">{video.title}</a>
+						<div class="flex flex-end">
+							<VideoSolid class="m-1" on:click={() => downloadVideo(video.videoId, 'video')} />
+							<FileMusicSolid class="m-1" on:click={() => downloadVideo(video.videoId, 'audio')} />
+						</div>
+					</ListgroupItem>
+				{/each}
+			</Listgroup>
+		</div>
+	</div>
+{/if}
 
-				<Hr class="my-4" />
-				<div class="flex flex-wrap items-center">
-					{#each videoInfo.keywords as keyword}
-						<Badge
-							border
-							color="red"
-							href="https://www.youtube.com/results?search_query={keyword}"
-							rel="noopener noreferrer"
-							target="_blank"
-							class="mr-2 mb-2"><SearchOutline class="w-2.5 h-2.5 mr-1.5" />{keyword}</Badge
+{#if searchInfo}
+	<!-- TODO:add Badge -->
+	<div class="grid grid-cols-1 py-8 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4">
+		{#each searchInfo as entry}
+			{#if entry?.videoId}
+				<div class="flex flex-col items-center h-full">
+					<Card img={entry.thumbnail.url} class="mb-4">
+						<h5
+							class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white line-clamp-1"
 						>
-					{/each}
-				</div>
-			</div>
-		</div>
-	{/if}
-
-	{#if playlistInfo}
-		<div class="max-w-3xl mx-auto mt-8">
-			<div class="flex flex-col items-center">
-				<img
-					src={playlistInfo.author.bestAvatar.url}
-					width={playlistInfo.author.bestAvatar.width}
-					height={playlistInfo.author.bestAvatar.height}
-					alt="CoverImage"
-				/>
-				<div>
-					<P>Playlist: {playlistInfo.title}</P>
-					<P>Author: {playlistInfo.author.name} | item: {playlistInfo.itemCount}</P>
-				</div>
-			</div>
-			<div class="border-solid border-2 border-red-500 mt-4">
-				<Listgroup>
-					{#each playlistInfo.videos as video}
-						<ListgroupItem class="flex items-center space-between">
-							<a href={video.url} class="m-1 flex-grow">{video.title}</a>
-							<div class="flex flex-end">
-								<VideoSolid class="m-1" on:click={() => downloadVideo(video.videoId, 'video')} />
-								<FileMusicSolid
-									class="m-1"
-									on:click={() => downloadVideo(video.videoId, 'audio')}
-								/>
-							</div>
-						</ListgroupItem>
-					{/each}
-				</Listgroup>
-			</div>
-		</div>
-	{/if}
-
-	{#if searchInfo}
-		<!-- TODO:add Badge -->
-		<div class="grid grid-cols-1 py-8 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4">
-			{#each searchInfo as entry}
-				{#if entry?.videoId}
-					<div class="flex flex-col items-center h-full">
-						<Card img={entry.thumbnail.url} class="mb-4">
-							<h5
-								class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white line-clamp-1"
-							>
-								<a href="https://www.youtube.com/watch?v={entry.videoId}">{entry.title}</a>
-							</h5>
-							<div class="flex items-center space-x-4">
-								<Avatar src={entry.author.thumbnail.url} rounded />
-								<div class="space-y-1 font-medium dark:text-white">
-									<div class="line-clamp-1">{entry.author.name}</div>
-									<div class="text-sm text-gray-500 dark:text-gray-400">
-										Published at {entry.published ? entry.published : 'unknown'}
-									</div>
+							<a href="https://www.youtube.com/watch?v={entry.videoId}">{entry.title}</a>
+						</h5>
+						<div class="flex items-center space-x-4">
+							<Avatar src={entry.author.thumbnail.url} rounded />
+							<div class="space-y-1 font-medium dark:text-white">
+								<div class="line-clamp-1">{entry.author.name}</div>
+								<div class="text-sm text-gray-500 dark:text-gray-400">
+									Published at {entry.published ? entry.published : 'unknown'}
 								</div>
 							</div>
-							<div class="text-sm text-gray-500 dark:text-gray-400">
-								{entry.viewCount} | {entry.duration}
-							</div>
-						</Card>
-						<div class="flex justify-center space-x-4">
-							<Button on:click={() => downloadVideo(entry.videoId, 'video')}>
-								<VideoSolid class="m-1" />
-								Download Video
-							</Button>
-							<Button on:click={() => downloadVideo(entry.videoId, 'audio')}>
-								<FileMusicSolid class="m-1" />
-								Download Audio
-							</Button>
 						</div>
+						<div class="text-sm text-gray-500 dark:text-gray-400">
+							{entry.viewCount} | {entry.duration}
+						</div>
+					</Card>
+					<div class="flex justify-center space-x-4">
+						<Button on:click={() => downloadVideo(entry.videoId, 'video')}>
+							<VideoSolid class="m-1" />
+							Download Video
+						</Button>
+						<Button on:click={() => downloadVideo(entry.videoId, 'audio')}>
+							<FileMusicSolid class="m-1" />
+							Download Audio
+						</Button>
 					</div>
-				{/if}
-			{/each}
-		</div>
-	{/if}
+				</div>
+			{/if}
+		{/each}
+	</div>
+{/if}
 
-	<FetchErrorModal />
-	<URLErrorModal />
+<FetchErrorModal />
+<URLErrorModal />
 
-	<Footer />
+<Footer />
