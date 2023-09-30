13bf8d6e9b817ecac24068375479eefdd95bdd5c
ysr9029 <56439343+Nich87@users.noreply.github.com>
2023-10-01 03:51:13 +0900

    chore:Added client to download options.
    Downloading in up to 720p is now possible.

--- src/routes/api/ytdl/download/+server.ts
+++ src/routes/api/ytdl/download/+server.ts
@@ -9,25 +9,26 @@ export const GET = async ({ url }: { url: URL }) => {
 
 	try {
 		if (type === 'video') {
-			//TODO: この状態だと360p onlyのためffmpegで動画と音声を組み合わせて一つのstreamを返してあげる。
 			const videoStream = await yt.download(id, {
 				type: 'video+audio',
 				quality: 'best',
-				format: 'mp4'
+				format: 'mp4',
+				client: 'ANDROID'
 			});
 
 			return new Response(videoStream, {
 				headers: {
 					'Content-Type': 'video/mp4'
 				}
-			});
+			})
 		}
 
 		if (type === 'audio') {
 			const audioStream = await yt.download(id, {
 				type: 'audio',
 				quality: 'best',
-				format: 'any'
+				format: 'any',
+				client: 'ANDROID'
 			});
 			return new Response(audioStream, {
 				headers: {
