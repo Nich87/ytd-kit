2fdaca5f2174b3c27967f3578c8490dc85cc706e
ysr9029 <56439343+Nich87@users.noreply.github.com>
2023-10-06 23:01:24 +0900

    refactor:variable made constant

--- src/lib/components/Spinner.svelte
+++ src/lib/components/Spinner.svelte
@@ -1,12 +1,13 @@
 <script lang="ts">
 	import { Spinner } from 'flowbite-svelte';
 	import { isLoading } from '$lib/store';
+	let size = "16";
 </script>
 
 {#if $isLoading}
 	<div
 		class="fixed flex justify-center items-center top-0 left-0 w-full h-full z-[1000] bg-black-opacity-80"
 	>
-		<Spinner size={'16'} />
+		<Spinner {size} />
 	</div>
 {/if}
