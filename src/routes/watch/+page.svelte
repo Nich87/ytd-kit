c254c0ee0a950d4e084788fac08f6a906b246663
ysr9029 <56439343+Nich87@users.noreply.github.com>
2023-11-09 23:35:52 +0900

    chore:Use Embed

--- src/routes/watch/+page.svelte
+++ src/routes/watch/+page.svelte
@@ -1,21 +1,23 @@
 <script lang="ts">
-	import { Player, Youtube } from '@vime/svelte';
+	import { Embed } from '@vime/svelte';
 	import { onMount } from 'svelte';
 	let id: string | null = '';
 	onMount(() => {
 		id = new URL(window.location.href).searchParams.get('v');
 	});
+
+	const params = {
+		autoplay: 1,
+		muted: 1,
+		controls: 0
+	};
 </script>
 
 <div id="container">
-	<Player controls>
-		<Youtube videoId={id} />
-	</Player>
+	<Embed
+		embedSrc={`https://www.youtube-nocookie.com/embed/${id}`}
+		origin="https://www.youtube-nocookie.com"
+		mediaTitle="Agent 327: Operation Barbershop"
+		{params}
+	/>
 </div>
-
-<style>
-	#container {
-		width: 100%;
-		height: 100vh;
-	}
-</style>
