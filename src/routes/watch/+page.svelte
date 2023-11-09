bf692ec732e40690b4851c3e48003b68045e4837
ysr9029 <56439343+Nich87@users.noreply.github.com>
2023-11-10 01:59:46 +0900

    chore:swap vime -> embed

--- src/routes/watch/+page.svelte
+++ src/routes/watch/+page.svelte
@@ -1,24 +1,15 @@
 <script lang="ts">
-	import { Embed } from '@vime/svelte';
 	import { onMount } from 'svelte';
 	let id: string | null = '';
 	onMount(() => {
+		document.body.classList.remove('p-4', 'space-y-8', 'container');
 		id = new URL(window.location.href).searchParams.get('v');
 	});
-
-	const params = {
-		autoplay: 1,
-		muted: 1,
-		controls: 0
-	};
 </script>
 
 {#if id !== null}
-	<div id="container">
-		<Embed
-			embedSrc={`https://www.youtube-nocookie.com/embed/${id}`}
-			origin="https://www.youtube-nocookie.com"
-			{params}
-		/>
-	</div>
+	<embed
+		src={`https://www.youtube-nocookie.com/embed/${id}?autoplay=1&muted=1&controls=0&modestbranding=0&loop=1`}
+		style="display:block;width:100%;height: 100vh;"
+	/>
 {/if}
