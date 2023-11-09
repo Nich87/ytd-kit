5418bfb93b1ccdbcabb686ac84429512109aa571
ysr9029 <56439343+Nich87@users.noreply.github.com>
2023-11-09 23:13:23 +0900

    feat:add ViewerPage

---
+++ src/routes/watch/+page.svelte
@@ -0,0 +1,25 @@
+<script lang="ts">
+	import {
+		Player,
+		Youtube,
+	} from '@vime/svelte';
+	import { onMount } from 'svelte';
+    let id: string | null = "" ;
+    onMount(() => {
+        id = new URL(window.location.href).searchParams.get('q');
+    })
+</script>
+
+
+<div id="container">
+	<Player controls>
+        <Youtube videoId={id} />
+	</Player>
+</div>
+
+<style>
+	#container {
+		width: 100%;
+		height: 100vh;
+	}
+</style>
